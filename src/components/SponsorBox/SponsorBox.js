4d0b1e2d81e2b78dd254a3419673bee535337c83
maridematte <marianag.dematte@gmail.com>
2018-08-19 12:48:45 -0300

    Changed sponsor doc name. The random numbers after it cannot be taken out bc they are part of the react hashing thingy

--- src/components/SponsorBox/SponsorBox.js
+++ src/components/SponsorBox/SponsorBox.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
 import styled from "styled-components";
 
-import sponsor from "./../../documents/BostonHacks_Sponsorship_V3.pdf";
+import sponsor from "./../../documents/BostonHacks_Sponsorship_Fall_2018.pdf";
 
 const DocButton = styled.button`
   background-color: #f05352;
