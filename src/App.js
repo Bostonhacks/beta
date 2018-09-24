3192027234e89d43e1c4550e194b6445ce5fbfa0
Lucas Watson <12654965+lkwatson@users.noreply.github.com>
2018-09-23 22:50:10 -0400

    Adds analytics, SEO (#36)

--- src/App.js
+++ src/App.js
@@ -21,8 +21,59 @@ class App extends Component {
           <meta itemProp="name" content="BostonHacks - November 10th & 11th" />
           <meta
             itemProp="description"
-            content="BostonHacks is Boston University's premier hackathon hosting 500 student from all over the US and Canada."
+            content="BostonHacks is Boston University's premier hackathon hosting 500 students from all over the US and Canada."
           />
+          <meta
+            itemProp="image"
+            content="https://static.bostonhacks.io/img/logo-no-text.png"
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-67825496-11"
+          />
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-67825496-11');
+            `}
+          </script>
+          <script type="application/ld+json">
+            {`
+              {
+                "@context": "http://schema.org",
+                "@type": "Event",
+                "name": "BostonHacks",
+                "startDate": "2018-11-10T9:00-04:00",
+                "location": {
+                  "@type": "Place",
+                  "name": "Boston University - George Sherman Union",
+                  "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "775 Commonwealth Avenue",
+                    "addressLocality": "Boston",
+                    "postalCode": "02215",
+                    "addressRegion": "MA",
+                    "addressCountry": "US"
+                  }
+                },
+                "image": "https://static.bostonhacks.io/img/logo-no-text.png",
+                "description": "BostonHacks is Boston University's premier hackathon hosting 500 student from all over the US and Canada.",
+                "endDate": "2018-11-11T15:00-04:00",
+                "offers": {
+                  "@type": "Offer",
+                  "url": "https://bostonhacks.io/",
+                  "price": "0",
+                  "priceCurrency": "USD",
+                  "description": "Free",
+                  "availability": "http://schema.org/InStock",
+                  "validFrom": "2017-01-20T16:20-08:00"
+                }
+              }
+            `}
+          </script>
         </Helmet>
 
         <a name="top" />
